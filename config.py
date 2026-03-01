8cb8f59af420bc074d4c69fa8d0f01dd4438f8e0
mohdzubairpatel <zubairpatel128@gmail.com>
2026-03-01 14:46:13 +0530

    Final DATABASE_URL setup

--- config.py
+++ config.py
@@ -3,9 +3,5 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host=os.environ.get("ep-fancy-cake-a107xc6m-pooler.ap-southeast-1.aws.neon.tech"),
-        database=os.environ.get("neondb"),
-        user=os.environ.get("neondb_owner"),
-        password=os.environ.get("npg_sLgMKp6uHbP3"),
-        port=os.environ.get("5432")
+        os.environ.get("postgresql://neondb_owner:npg_sLgMKp6uHbP3@ep-fancy-cake-a107xc6m-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
     )
